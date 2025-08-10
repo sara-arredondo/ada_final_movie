41b338215beb4eb7bed6c254dcca76aaa5375a33
SaraArredondo <sara.arredondo.giraldo@gmail.com>
2025-08-09 19:54:11 -0500

    se termina la estructura y enrutado del proyecto

---
+++ src/hooks/useMovies.js
@@ -0,0 +1,12 @@
+function useMovies() {
+
+    const [movies, setMovies] = useState()
+   
+    const data = {
+        movies
+    }
+
+    return data
+}
+
+export default useMovies
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,17 +4,23 @@ import DetailMovie from "../views/DetailMovie";
 import FullPopularMovies from "../views/FullPopularMovies";
 import FullUltimosLanzamientosMovies from "../views/FullUltimosLanzamientosMovies";
 import Buscador from "../views/Buscador";
+import FavoritesMovies from "../views/FavoritesMovies"
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 
 function Router() {  
   return <BrowserRouter>
+    <NavBar />
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies/:id" element={< DetailMovie/>} />
+        <Route path="movie/:id" element={< DetailMovie/>} />
         <Route path="/PopularMovies" element={< FullPopularMovies/>} />
         <Route path="/UltimosLanzamientos" element={< FullUltimosLanzamientosMovies/>} />
+        <Route path="/Favorites" element={< FavoritesMovies/>} />
         <Route path="/buscador" element={< Buscador/>} />
     </Routes>
+    <Footer />
   </BrowserRouter>;
 
 }
