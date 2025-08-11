fd073ba6ece7661e3b2e1e47865625f730e1e40c
SaraArredondo <sara.arredondo.giraldo@gmail.com>
2025-08-11 12:17:08 -0500

    se agregan estilos globales, se crea un theme de MU

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 import App from "./App.jsx";
-import "./reset.css";
-import "./normalize.css";
-import CssBaseline from '@mui/material/CssBaseline';
+import theme from "./theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <CssBaseline />
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
