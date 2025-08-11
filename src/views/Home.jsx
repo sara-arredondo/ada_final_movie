34dd999a61bd9995b7a821ac76709ceb9e71f8c5
SaraArredondo <sara.arredondo.giraldo@gmail.com>
2025-08-11 14:05:32 -0500

    se ajusta el color del fondo del home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,10 +3,15 @@ import HeroContainer from '../components/HeroContainer'
 import ListMoviesContainer from '../components/ListMoviesContainer'
 
 const Home = () => (
-  <>
+  <div style={{
+      backgroundColor: "#272727",
+      color: "#fff",          
+      minHeight: "100vh"     
+    }}
+  >
     <HeroContainer />
     <ListMoviesContainer />
-  </>
+  </div>
 );
 
-export default Home
+export default Home;
