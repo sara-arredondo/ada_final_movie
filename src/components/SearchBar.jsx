9884555fe99ad1bd75614347b645794639283035
SaraArredondo <sara.arredondo.giraldo@gmail.com>
2025-08-13 17:59:49 -0500

    se arregla problema en el buscador y se ajusta el estilo del buscador, decido cambiar de paleta de colores

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,6 +3,40 @@ import { TextField } from "@mui/material";
 export default function SearchBar({ value, onChange, autoFocus = false }) {
   return (
     <TextField
+
+    slotProps={{
+        inputLabel: {
+        sx: {
+            color: "white",
+            "&.Mui-focused": {
+            color:'#f35a5d',
+            }
+        }
+        },
+        input: {
+        sx: {
+            color: "white"
+        }
+        }
+    }}
+    
+    
+    sx={{
+        '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+             borderColor: '#f35a5d',
+        },
+        '&:hover fieldset': {
+            borderColor: '#98c7f3',
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: '#f35a5d',
+        },
+        
+        },
+        }}
+
+        
       fullWidth
       label="Buscar película"
       placeholder="Ej: Dune, Batman…"
